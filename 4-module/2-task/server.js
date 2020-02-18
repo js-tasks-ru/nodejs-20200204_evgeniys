7c7fbc721345275b62fff3a7c3835bd108f6a892
eugens <sorogin@gmail.com>
2020-02-19 00:19:02 +0200

    solution 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -9,9 +11,52 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const ws = fs.createWriteStream(filepath, {flags: 'wx'});
+  const limitStream = new LimitSizeStream({limit: 1000000});
+
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('included directory is not supported');
+  }
+
+  req
+      .on('error', (err) => {
+        console.log('An error has been occured');
+        res.statusCode = 500;
+        res.end('Internal server error occured');
+      })
+      .on('close', () => {
+        if (req.readableEnded) return;
+        ws.destroy();
+        fs.access(filepath, fs.constants.F_OK, (err)=>{
+          if (err) {
+            return;
+          }
+          fs.unlinkSync(filepath);
+        });
+      });
+
   switch (req.method) {
     case 'POST':
-
+      req
+          .pipe(limitStream)
+          .on('error', (err) => {
+            if (err.code === 'LIMIT_EXCEEDED') {
+              res.statusCode = 413;
+              res.end('File size limit exceeded');
+            }
+          })
+          .pipe(ws)
+          .on('error', (err) => {
+            if (err.code === 'EEXIST') {
+              res.statusCode = 409;
+              res.end('File already exist!');
+            }
+          })
+          .on('close', () => {
+            res.statusCode = 201;
+            res.end('File has been uploaded');
+          });
       break;
 
     default:
