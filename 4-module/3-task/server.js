1ab3e1580f29344c0478ff78386849664ace7c1f
eugens <sorogin@gmail.com>
2020-02-20 21:59:05 +0200

    solution 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -9,9 +10,29 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('included directory is not supported');
+  }
+  try {
+    fs.accessSync(filepath, fs.constants.F_OK);
+  } catch (err) {
+    if (err.code === 'ENOENT'){
+      res.statusCode = 404;
+      res.end('File does not exist');
+    }
+  }
+
   switch (req.method) {
     case 'DELETE':
-
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('An error occured during delete');
+        }
+        res.statusCode = 200;
+        res.end('File successfully deleted');
+      });
       break;
 
     default:
