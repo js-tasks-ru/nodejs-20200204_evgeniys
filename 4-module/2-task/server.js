8f0873c44f13c4b1c69b08def3df6c0b2aa4fcd7
eugens <sorogin@gmail.com>
2020-02-25 18:52:39 +0200

    add solution 5-module 1-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -11,47 +11,46 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
-  const ws = fs.createWriteStream(filepath, {flags: 'wx'});
-  const limitStream = new LimitSizeStream({limit: 1000000});
-
-  if (pathname.includes('/') || pathname.includes('..')) {
-    res.statusCode = 400;
-    res.end('included directory is not supported');
-  }
-
-  req
-      .on('error', (err) => {
-        res.statusCode = 500;
-        res.end('Internal server error occured');
-      })
-      .on('close', () => {
-        if (req.complete) return;
-        ws.destroy();
-        fs.unlinkSync(filepath);
-      });
-  ws.on('error', (err) => {
-    if (err.code === 'EEXIST') {
-      res.statusCode = 409;
-      res.end('File already exist!');
-    }
-    res.statusCode = 500;
-    res.end('Internal Error');
-  }).on('close', () => {
-    res.statusCode = 201;
-    res.end('File has been uploaded');
-  });
-
   switch (req.method) {
     case 'POST':
-      req
-          .pipe(limitStream)
-          .on('error', (err) => {
-            if (err.code === 'LIMIT_EXCEEDED') {
-              res.statusCode = 413;
-              res.end('File size limit exceeded');
-            }
-          })
-          .pipe(ws);
+
+      if (path.basename(pathname) !== pathname) {
+        res.statusCode = 400;
+        res.end('Invalid path');
+        return;
+      }
+      const writeFile = fs.createWriteStream(filepath, {flags: 'wx'});
+      const limitedStream = new LimitSizeStream({limit: 1048576});
+      req.pipe(limitedStream).pipe(writeFile);
+
+      limitedStream.on('error', (err) => {
+        if (err.code === 'LIMIT_EXCEEDED') {
+          res.statusCode = 413;
+          res.end('Max file size exceeded');
+          return;
+        } else {
+          res.statusCode = 500;
+          res.end('Internal Error');
+        }
+      });
+      writeFile.on('error', (err) => {
+        if (err.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('File already exist');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal Error');
+        }
+      });
+      writeFile.on('close', () => {
+        res.statusCode = 201;
+        res.end('File was written');
+      });
+      req.on('close', () => {
+        if (req.complete) return;
+        writeFile.destroy();
+        fs.unlinkSync(filepath);
+      });
       break;
 
     default:
